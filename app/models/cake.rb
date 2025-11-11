95386408a0c64889e102fe99f07ee0c12314469d
CaryuterSal <20243ds158@utez.edu.mx>
2025-11-11 10:23:50 -0600

    fix: enum prefix

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -4,37 +4,37 @@ class Cake < ApplicationRecord
     chocolate: 1,
     fresa: 2,
     red_velvet: 3
-  }
+  }, _prefix: true
 
   enum glaseado: {
     crema: 0,
     chocolate: 1,
     mantequilla: 2
-  }
+  }, _prefix: true
 
   enum topping: {
     chispas: 0,
     fruta: 1,
     nuez: 2,
     ninguno: 3
-  }
+  }, _prefix: true
 
   enum relleno: {
     mermelada: 0,
     crema: 1,
     nutella: 2,
     ninguno: 3
-  }
+  }, _prefix: true
 
   enum tamanio: {
     chico: 0,
     mediano: 1,
     grande: 2
-  }
+  }, _prefix: true
 
   enum forma: {
     redondo: 0,
     cuadrado: 1,
     corazon: 2
-  }
+  }, _prefix: true
 end
